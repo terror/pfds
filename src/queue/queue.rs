5476b767b90d424d2b8cfd15234c3e252014e85c
Liam <liam@scalzulli.com>
2025-08-11 23:05:09 -0400

    Add `Iterator` supertrait requirement to `Queue` trait

--- src/queue/queue.rs
+++ src/queue/queue.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-pub trait Queue<'a, T: Clone + Send + Sync + 'a> {
+pub trait Queue<'a, T: Clone + Send + Sync + 'a>: Iterator<Item = T> {
   /// Removes and returns a new queue without the front element.
   fn dequeue(self) -> Result<Self, Error>
   where
